cddf521dab0da71dc3301794b9219922cec87240
Faiiss <33278@ma-web.nl>
2022-05-11 14:58:27 +0200

    Update periode4 2022

--- periode4/F2M4BO-MakeitRain/java/script.js
+++ periode4/F2M4BO-MakeitRain/java/script.js
@@ -2,17 +2,17 @@ function imgSlider(anything){
     document.querySelector('.Jordan1').src = anything;
 }
 
-function changeCirclekleur(color){
+function changeCirclecolor(color){
     const circle = document.querySelector('.circle')
     circle.style.background = color;
 }
 
-function changeTextkleur(color){
+function changeTextcolor(color){
     const textkleur = document.querySelector('.texth')
     textkleur.style.color = color;
 }
 
-function changebuttonkleur(color){
+function changebuttoncolor(color){
     const kleurbutton = document.querySelector('.button1')
     kleurbutton.style.background = color;
 }
